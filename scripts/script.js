44955cf225a9560ebabecbac85ce7a6905cc9cff
Mohammad Raihan <baga.mohammad@icloud.com>
2026-02-04 07:41:26 +0800

    almost done

--- scripts/script.js
+++ scripts/script.js
@@ -1,54 +1,54 @@
+// visual indicators
+const waterLevel = document.querySelector('#water');
+const waterConsumed = document.querySelector('#consumed-vol');
+const waterRemaining = document.querySelector('#remaining-vol');
+
+// buttons
 const addBtn = document.querySelector('#add-btn');
-const removeBtn = document.querySelector('#minus-btn');
+const removeBtn = document.querySelector('#remove-btn');
 const resetBtn = document.querySelector('#reset-btn');
 
-const consumedVol = document.querySelector('#consumed-vol');
-const remainingVol = document.querySelector('#remaining-vol');
-const waterPercentage = document.querySelector('#water');
-
+// assigning values
 let consumed = 0,
     remaining = 2500,
     percentage = 0;
 
-const maxConsume = 2500,
-      minRemaining = 0;
-
 addBtn.addEventListener('click', addWater);
 removeBtn.addEventListener('click', removeWater);
-resetBtn.addEventListener('click', reset);
+resetBtn.addEventListener('click', resetVal);
 
 function addWater() {
+  if (consumed === 2500) return;
+
   consumed += 250;
   remaining -= 250;
-  percentage = (consumed / 2500) * 100;
 
-  consumedVol.textContent = `${consumed} mL`;
-  remainingVol.textContent = `${remaining} mL`;
-  waterPercentage.style.height = `${percentage}%`;
+  updateVal();
+  // percentage = (consumed / 2500) * 100; 
 }
 
 function removeWater() {
+  if (consumed === 0) return;
+
   consumed -= 250;
   remaining += 250;
-  percentage = (consumed / 2500) * 100;
+  // percentage = (consumed / 2500) * 100;
 
-  consumedVol.textContent = `${consumed} mL`;
-  remainingVol.textContent = `${remaining} mL`;
-  waterPercentage.style.height = `${percentage}%`;
-
-  if (remaining === minRemaining) {
-    removeBtn.disabled = true;
-  } else {
-    removeBtn.disabled = false;
-  }
+  updateVal();
 }
 
-function reset() {
+function resetVal() {
   consumed = 0;
   remaining = 2500;
   percentage = 0;
+
+  updateVal();
+}
+
+function updateVal() {
+  percentage = (consumed / 2500) * 100;
   
-  consumedVol.textContent = `${consumed} mL`;
-  remainingVol.textContent = `${remaining} mL`;
-  waterPercentage.style.height = `${percentage}%`;
+  waterConsumed.textContent = `${consumed} mL`;
+  waterRemaining.textContent = `${remaining} mL`;
+  waterLevel.style.height = `${percentage}%`;
 }
