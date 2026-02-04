3dbeb5b3edbff1420ce1e8a430af40f695599c7c
Mohammad Raihan <baga.mohammad@icloud.com>
2026-02-04 08:12:20 +0800

    cleaner

--- scripts/script.js
+++ scripts/script.js
@@ -9,9 +9,8 @@ const removeBtn = document.querySelector('#remove-btn');
 const resetBtn = document.querySelector('#reset-btn');
 
 // assigning values
-let consumed = 0,
-    remaining = 2500,
-    percentage = 0;
+const MAX = 2500;
+let consumed = 0;
 
 addBtn.addEventListener('click', addWater);
 removeBtn.addEventListener('click', removeWater);
@@ -21,7 +20,6 @@ function addWater() {
   if (consumed === 2500) return;
 
   consumed += 250;
-  remaining -= 250;
 
   updateVal();
 }
@@ -30,21 +28,19 @@ function removeWater() {
   if (consumed === 0) return;
 
   consumed -= 250;
-  remaining += 250;
 
   updateVal();
 }
 
 function resetVal() {
   consumed = 0;
-  remaining = 2500;
-  percentage = 0;
 
   updateVal();
 }
 
 function updateVal() {
-  percentage = (consumed / 2500) * 100;
+  const remaining = MAX - consumed;
+  const percentage = (consumed / 2500) * 100;
   
   waterConsumed.textContent = `${consumed} mL`;
   waterRemaining.textContent = `${remaining} mL`;
